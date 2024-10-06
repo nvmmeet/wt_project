dc2a5c2396138f1e436c0450785a58bd89418918
nvmmeet <meetweb00@gmail.com>
2024-10-07 04:15:15 +0530

    s

--- album.php
+++ album.php
@@ -2,11 +2,14 @@
 include "includes/sessions/false.php";
 include "includes/config.php";
 
+$username = $_SESSION["username"];
 $album_id = intval($_GET['album']);
+
 if (!$album_id) {
     $error = "Bad Request!";
 }
 
+// Fetch album and song details
 $albumQuery = $pdo->prepare("
     SELECT 
         a.album_id,
@@ -14,13 +17,16 @@ $albumQuery = $pdo->prepare("
         a.album_pic_url,
         s.song_id,
         s.song_name,
-        s.song_pic_url
+        s.song_pic_url,
+        u.name AS artist_name
     FROM 
         albums a
     LEFT JOIN 
         album_songs als ON a.album_id = als.album_id
     LEFT JOIN 
         songs s ON als.song_id = s.song_id
+    LEFT JOIN 
+        users u ON s.user_id = u.user_id
     WHERE 
         a.album_id = :album_id
 ");
@@ -32,6 +38,15 @@ if (empty($albumData)) {
 } else {
     $album = reset($albumData);
 }
+
+// Fetch user's playlists for the "Add to Playlist" feature
+$playlistQuery = $pdo->prepare("
+    SELECT playlist_id, playlist_name 
+    FROM playlists 
+    WHERE user_id = (SELECT user_id FROM users WHERE username = :username)
+");
+$playlistQuery->execute(['username' => $username]);
+$playlists = $playlistQuery->fetchAll();
 ?>
 
 <html>
@@ -43,6 +58,7 @@ if (empty($albumData)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/sidebar.css" />
     <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/cards.css" />
 </head>
 
 <body>
@@ -59,22 +75,47 @@ if (empty($albumData)) {
                 </div>
 
                 <h2>Songs</h2>
-                <table class="songs">
-                    <tr>
-                        <th>#</th>
-                        <th>Song Name</th>
-                        <th>Play</th>
-                    </tr>
+                <div class="song-cards">
                     <?php foreach ($albumData as $song): ?>
                         <?php if ($song['song_id']): ?>
-                            <tr>
-                                <td><img src="" alt="song_pic"></td>
-                                <td><?= htmlspecialchars($song['song_name']) ?></td>
-                                <td><a href="#" class="play-button"><i class="bi bi-caret-right-fill"></i> Play</a></td>
-                            </tr>
+                            <?php
+                            $songImage = $song['song_pic_url'] !== 'default'
+                                ? 'uploads/images/songs/' . htmlspecialchars($song['song_pic_url'])
+                                : 'uploads/images/songs/emptysong.jpg';
+                            ?>
+                            <div class='song-card'>
+                                <img src='<?= $songImage ?>' alt='song' />
+                                <span><?= htmlspecialchars($song['song_name']) ?></span>
+                                <p><?= htmlspecialchars($song['artist_name']) ?></p>
+                                <div class='play-button'>
+                                    <i class='bi bi-caret-right-fill'></i>
+                                </div>
+                                <input type='checkbox' id='dropdown<?= $song['song_id'] ?>' />
+                                <label for='dropdown<?= $song['song_id'] ?>'><i class='bi bi-three-dots-vertical'></i></label>
+                                <div class='song-card-dropdown'>
+                                    <div class='dropdown-item sub-dropdown hov'>
+                                        Add to Playlist
+                                        <div class='sub-dropdown-content'>
+                                            <?php foreach ($playlists as $playlist): ?>
+                                                <!-- Check if song is already in the playlist -->
+                                                <div class='dropdown-item'>
+                                                    <a href='addtopl.php?song=<?= $song['song_id'] ?>&playlist=<?= $playlist['playlist_id'] ?>'>
+                                                        <?= htmlspecialchars($playlist['playlist_name']) ?>
+                                                    </a>
+                                                </div>
+                                            <?php endforeach; ?>
+                                        </div>
+                                    </div>
+                                    <div class='dropdown-item'>
+                                        <a href='addtofav.php?song=<?= $song['song_id'] ?>'>
+                                            Add to Favourites
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         <?php endif; ?>
                     <?php endforeach; ?>
-                </table>
+                </div>
             </section>
         <?php endif; ?>
     </main>
